96c4a87d3bbb0b12969662d5b79c3fe7510aecb4
zhujianfeng0309 <358008157@qq.com>
2019-06-09 22:58:54 +0800

    启动

--- server-web/src/main/java/self/person/study/SimpleMoment.java
+++ server-web/src/main/java/self/person/study/SimpleMoment.java
@@ -2,11 +2,12 @@ package self.person.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Created by Administrator on 2019/6/4.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SimpleMoment {
         public static void main(String[] args) {
             SpringApplication.run(SimpleMoment.class, args);
